02fe38a8572d719d5fa64d95a3475d1fe0daefc1
doitnow22 <yiheeae@naver.com>
2022-06-16 15:01:23 +0900

    file upload 3:01

---
+++ 11while.py
@@ -0,0 +1,13 @@
+#while.py 문서
+# while 조건 :
+#     처리할 문서
+#     무한루프일때 if 조건 : break
+# 문제 1 반복문 사용안하고 1~5 출력 합격
+# 1 2 3 4 5 합계 = 15
+# su hao
+# print함수는 자동라인개행 enter 기능 포함
+
+su, hap = 0,0
+su =su+1
+
+print(su)
--- aaa.py
+++
@@ -1,4 +0,0 @@
-name='고길동'
-print(name)
-print('여름 =', name)
-print('-', *70)
